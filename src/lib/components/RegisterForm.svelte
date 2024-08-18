01b9588992b907cfe8ab8f03f1b1df52de1ad1c1
Juandld <juanda.lopez3011@gmail.com>
2024-08-18 12:02:56 -0500

    skeleton+auth+firbase

---
+++ src/lib/components/RegisterForm.svelte
@@ -0,0 +1,49 @@
+<script>
+	import { auth } from '$lib/firebase/config';
+	import { createUserWithEmailAndPassword } from 'firebase/auth';
+
+	let email = '';
+	let name = '';
+	let password = '';
+	let password2 = '';
+
+
+    const submit = () => {
+	createUserWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+			// Signed up
+			const user = userCredential.user;
+			
+		})
+		.catch((error) => {
+			const errorCode = error.code;
+			const errorMessage = error.message;
+			// ..
+		});
+    } 
+</script>
+
+<div class="card variant-filled w-2/3 p-4">
+	<form action="">
+		<label class="label">
+			<span>Email</span>
+			<input class="input text-white" type="email" id="email" bind:value={email} />
+		</label>
+        <label class="label">
+			<span>Name</span>
+			<input class="input text-white" type="text" id="name" bind:value={name} />
+		</label>
+		<label class="label"
+			>Password
+			<input class="input text-white" type="password" id="password" bind:value={password} />
+		</label>
+        <label class="label"
+			>Confirm Password
+			<input class="input text-white" type="password" id="password2" bind:value={password2} />
+		</label>
+
+		<button class="btn variant-filled-surface m-2" on:click={submit}
+			>Register
+		</button>
+	</form>
+</div>
---
+++ src/routes/register/+page.svelte
@@ -0,0 +1,8 @@
+<script>
+    import RegisterForm from "$lib/components/RegisterForm.svelte";
+</script>
+
+<div>
+    <RegisterForm />
+</div>
+
